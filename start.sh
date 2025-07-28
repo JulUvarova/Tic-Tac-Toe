9ba99100532bd80d3e1ec9d00f9799189b7bc1ca
trumpecy <jyli_91@mail.ru>
2025-07-28 20:30:15 +0300

    add db comment

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ sudo docker compose up -d --build
 
 if [ $? -eq 0 ]; then
     echo "Контейнеры успешно запущены. Открывай localhost:8080"
+    echo "Подключение к БД:  jdbc:postgresql://localhost:5433/tic-tac-toe-db"
 else
     echo "Ошибка запуска контейнеров."
 fi
